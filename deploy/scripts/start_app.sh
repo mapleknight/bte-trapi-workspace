1c92327b258451b4e4beec96952142445723ac5d
oacob1 <oacob1@gmail.com>
2021-09-24 12:40:41 -0400

    updated start app script

--- deploy/scripts/start_app.sh
+++ deploy/scripts/start_app.sh
@@ -6,7 +6,7 @@ export $(egrep -v '^#' /env/.env)
 printenv | grep REDIS
 
 # start app
-pm2-runtime /home/node/app/process.yml
+pm2-runtime bte-pm2.json --env prodci_nocache --only bte-trapi
 
 # this line is added to stop container completes with exit code 0
 # tail -f /dev/null
