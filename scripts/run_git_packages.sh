837284c6f6f9a64ffe8b1a1ba520a8184bbaa268
tokebe <43009413+tokebe@users.noreply.github.com>
2022-03-04 17:00:47 -0500

    build: standardize scripts to external packages.txt

--- scripts/run_git_packages.sh
+++ scripts/run_git_packages.sh
@@ -1,16 +1,6 @@
 current_folder=`pwd`
 
-module_directories=(
-    "./packages/@biothings-explorer/bte-trapi"
-    "./packages/@biothings-explorer/query_graph_handler"
-    "./packages/biolink-model"
-    "./packages/biomedical_id_resolver"
-    "./packages/@biothings-explorer/smartapi-kg"
-    "./packages/@biothings-explorer/call-apis"
-    "./packages/@biothings-explorer/api-response-transform"
-)
-
-for module_dir in "${module_directories[@]}"
+while read -r url module_dir
 do
     cd "$module_dir"
     echo
@@ -18,4 +8,4 @@ do
     echo "-----"
     git $*
     cd $current_folder
-done
+done < scripts/packages.txt
