edd31ecebee91d8887b1463294b829ccb66a9778
Ke Wang <mapleknight@users.noreply.github.com>
2022-03-10 16:21:35 -0500

    Update start_app.sh

--- deploy/scripts/start_app.sh
+++ deploy/scripts/start_app.sh
@@ -6,7 +6,7 @@ export $(egrep -v '^#' /env/.env)
 printenv | grep REDIS
 
 # start app
-pm2-runtime bte-pm2.json --env prodci_nocache --only bte-trapi
+pm2-runtime bte-pm2.json --env env_prodci --only bte-trapi
 
 # this line is added to stop container completes with exit code 0
 # tail -f /dev/null
