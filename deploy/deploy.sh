c813a4c0b3159d9245ab2fc93e3d0083237e24e7
Pouyan Ahmadi <pahmadi@gmu.edu>
2022-03-11 14:08:37 -0500

    fix: sleep timer updated

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,6 +19,6 @@ done
 
 # for CI, need to remove previous deployment since the taint and tolleration will only allow one deployment exists
 helm -n ${namespace} uninstall ${projectName} 
-sleep 10
+sleep 30
 # deploy helm chart
 helm -n ${namespace} upgrade --install ${projectName} -f values-bte.yaml ./
